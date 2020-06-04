d4f931c68553784c4e7b998aa640f27ebb2584d9
littlemonster00 <sang.lequang94@gmail.com>
2020-06-05 00:11:06 +0700

    DS: BST add delete node and find node

--- java/Trees/BinaryTree.java
+++ java/Trees/BinaryTree.java
@@ -38,7 +38,41 @@ public class BinaryTree{
 		return current; // equal not add key to tree
 
 	}	
-
+	// Find node
+	public Node findNode(Node current, int value) {
+		if(current == null) {
+			return null;
+		}
+		if(current.getValue() == value) return current;
+		if(value > current.getValue()) return findNode(current.right, value);
+		return findNode(current.left, value);
+	}
+	
+	// Delete node
+	public Node findLeftMost(Node current) {
+		return current.left == null ? current : findLeftMost(current.left);
+	}
+	public Node deleteNode(Node current, int value){
+		if(current == null) return null;
+		
+		if(current.getValue() == value) {	
+			// 1. Node is leaf
+			if(current.left == null && current.right == null) return null;
+			// 2. Node have a single child
+			else if(current.left == null) return current.right;
+			else if(current.right == null) return current.left;
+			// 3. Node have 2 childs
+			else {
+				Node p = findLeftMost(current.right);
+				current.setValue(p.getValue());
+				deleteNode(current.right, p.getValue());
+				return current;
+			}
+		}
+		if(value > current.getValue()) current.right = deleteNode(current.right, value);
+		if(value < current.getValue()) current.left = deleteNode(current.left, value);
+		return current; // current deleted
+	}
 	// Add key
 	public void add(int value){
 		root = addRecursive(root, value);
@@ -65,16 +99,23 @@ public class BinaryTree{
 	public static void main(String[] args) {
 		BinaryTree t = new BinaryTree();
 		// Add node for tree
+		t.add(5);
 		t.add(3);
+		t.add(7);
 		t.add(2);
 		t.add(4);
-		t.add(9);
-		t.add(5);
-		t.add(1);
-		t.add(0);
-		t.add(7);	
+		t.add(6);
+		t.add(8);
 		// Traversal tree
-		t.NLR(t.root);	
+		//t.NLR(t.root);	
+
+		t.LNR(t.root);
+		
+		// find node
+		Node p = t.findNode(t.root, 5);
+		if(p != null)
+			System.out.println("Find: " + p.getValue());
+		t.deleteNode(t.root, 5);
 		t.LNR(t.root);
 	}
 }
