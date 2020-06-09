36ff0efa015b6a210b568a1ce56fe2e64f4b8e89
littlemonster00 <sang.lequang94@gmail.com>
2020-06-09 20:21:40 +0700

    CI update fomat code

--- coding-interview/findAllDuplicatedIII.java
+++ coding-interview/findAllDuplicatedIII.java
@@ -1,26 +1,27 @@
-import java.util.*; 
-class Solution{
-		public static boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-	    HashMap<Long, Long> map = new HashMap<>();
-	    for(int i = 0; i < nums.length; i++) {
-				long remappedNum = (long) nums[i] - Integer.MIN_VALUE; // cover to positve number
-				long bucket = remappedNum /((long)t + 1); // t + 1 avoid devide by 0
-				if(map.containsKey(bucket) 
-					||map.containsKey(bucket - 1) &&  remappedNum - map.get(bucket - 1) <= t
-					||map.containsKey(bucket + 1) &&  map.get(bucket + 1) - remappedNum <= t){
-					return true;
-				}
-				if(map.entrySet().size() >= k) {
-					long lastBucket = ((long) nums[i - k] - Integer.MIN_VALUE) / ((long)t + 1);
-					map.remove(lastBucket);
-				}
-				map.put(bucket, remappedNum); // replace the duplicated key
-	    }
-	    return false;
+import java.util.*;
+
+class Solution {
+	public static boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+		HashMap<Long, Long> map = new HashMap<>();
+		for (int i = 0; i < nums.length; i++) {
+			long remappedNum = (long) nums[i] - Integer.MIN_VALUE; // cover to positve number
+			long bucket = remappedNum / ((long) t + 1); // t + 1 avoid devide by 0
+			if (map.containsKey(bucket) || map.containsKey(bucket - 1) && remappedNum - map.get(bucket - 1) <= t
+					|| map.containsKey(bucket + 1) && map.get(bucket + 1) - remappedNum <= t) {
+				return true;
+			}
+			if (map.entrySet().size() >= k) {
+				long lastBucket = ((long) nums[i - k] - Integer.MIN_VALUE) / ((long) t + 1);
+				map.remove(lastBucket);
+			}
+			map.put(bucket, remappedNum); // replace the duplicated key
+		}
+		return false;
 	}
-	public static void main(String[] args){
-		int[] nums = {3, -3, 1, 0, 1};
-		boolean check =	containsNearbyAlmostDuplicate(nums, 1, 2);
+
+	public static void main(String[] args) {
+		int[] nums = { 3, -3, 1, 0, 1 };
+		boolean check = containsNearbyAlmostDuplicate(nums, 1, 2);
 		System.out.println(check);
 	}
 }
--- coding-interview/quickSelect.java
+++ coding-interview/quickSelect.java
@@ -4,13 +4,13 @@
 // k = 3
 // Output: 7
 
-class QuickSelect{
-  public static int partition(int[] arr, int low, int high){
+class QuickSelect {
+	public static int partition(int[] arr, int low, int high) {
 		// Considers last element as pivot
 		int pivot = arr[high], pivotloc = low;
-		for(int i = low; i <= high - 1; i++){
+		for (int i = low; i <= high - 1; i++) {
 			// inserting elements of less value to the left of the pivot location
-			if(arr[i] < pivot){
+			if (arr[i] < pivot) {
 				int temp = arr[i];
 				arr[i] = arr[pivotloc];
 				arr[pivotloc] = temp;
@@ -26,34 +26,32 @@ class QuickSelect{
 
 	// find the kth position(of the sorted array
 
-	public static int kthSmallest(int[] arr, int low, int high, int k){
+	public static int kthSmallest(int[] arr, int low, int high, int k) {
 		// fin the partition
 		int partition = partition(arr, low, high);
 		// if partition value is equal to the kth postion return value at k
-		if(partition == k) return arr[partition];
+		if (partition == k)
+			return arr[partition];
 		// if partition value is less than kth position search right side of the arr
-		else if(partition < k) return kthSmallest(arr, partition + 1, high, k);
-		else return kthSmallest(arr, low, partition - 1, k);
-			
+		else if (partition < k)
+			return kthSmallest(arr, partition + 1, high, k);
+		else
+			return kthSmallest(arr, low, partition - 1, k);
+
 	}
-	public static void main(String[] args){
-		int[] array = new int[]{7, 10, 4, 3, 20, 15}; 
-    int[] arraycopy = new int[]{10, 4, 5, 8, 6, 11, 26}; 
-              
-    int kPosition = 3; 
-    int length = array.length; 
-      
-    if(kPosition > length) 
-    { 
-        System.out.println("Index out of bound"); 
-    } 
-    else
-    { 
-       // find kth smallest value 
-       System.out.println("K-th smallest element in array : " +  
-                                kthSmallest(array, 0, length - 1, 
-                                                          kPosition - 1));  
-     }  	
+
+	public static void main(String[] args) {
+		int[] array = new int[] { 7, 10, 4, 3, 20, 15 };
+		int[] arraycopy = new int[] { 10, 4, 5, 8, 6, 11, 26 };
+
+		int kPosition = 3;
+		int length = array.length;
+
+		if (kPosition > length) {
+			System.out.println("Index out of bound");
+		} else {
+			// find kth smallest value
+			System.out.println("K-th smallest element in array : " + kthSmallest(array, 0, length - 1, kPosition - 1));
+		}
 	}
 }
-
