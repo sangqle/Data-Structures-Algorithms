f8e942de4922cc2e0b394219198243f591df214f
littlemonster00 <sang.lequang94@gmail.com>
2020-06-30 10:48:25 +0700

    CI Add same-tree solution with Recursion approach fix code clean

--- coding-interview/same-tree.java
+++ coding-interview/same-tree.java
@@ -1,27 +1,34 @@
 /**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode() {} TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) { this.val = val; this.left
+ * = left; this.right = right; } }
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-       if(p != null && q != null) {
-           if(p.val == q.val) {
-               return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-           } else {
-               return false;
-           }
-       } else if(p == null && q == null) return true;
-       else return false;
+        if (p != null && q != null) {
+            if (p.val == q.val) {
+                return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+            } else {
+                return false;
+            }
+        } else if (p == null && q == null)
+            return true;
+        else
+            return false;
+    }
+
+    // Version code clean
+    public boolean isSameTree2(TreeNode p, TreeNode q) {
+        if (p == null && q == null)
+            return true;
+        if (p == null || q == null)
+            return false;
+        if (p.val == q.val) {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        } else {
+            return false;
+        }
+
     }
 }
